7614439c63e9669129d2c701434ca703602e92af
crhistian33 <crhistian.pj@gmail.com>
2026-03-20 13:52:40 -0500

    fix(Module): fix app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,6 @@ import { ProductsModule } from './modules/products/products.module';
 import { SiteConfigModule } from './modules/site-config/site-config.module';
 import { HeroSlidesModule } from './modules/hero-slides/hero-slides.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { PagesModule } from './modules/pages/pages.module';
 
 @Module({
   imports: [
@@ -29,7 +28,6 @@ import { PagesModule } from './modules/pages/pages.module';
     ProductsModule,
     SiteConfigModule,
     HeroSlidesModule,
-    PagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
