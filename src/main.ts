1499f07a83ae93f6781b622157130cc7b29ebf36
crhistian33 <crhistian.pj@gmail.com>
2026-07-20 19:10:50 -0500

    Fix route admin CMS in enableCors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
       'https://e-werd.pages.dev',
       /^https:\/\/.*\.e-werd\.pages\.dev$/, // previews
       'http://localhost:4200',
-      'https://admin-werd-production.up.railway.app/',
+      'https://admin-werd-production.up.railway.app',
       // 'https://admin-werd-production.up.railway.app',
       // 'https://e-werd-production-ee9d.up.railway.app',
     ],
